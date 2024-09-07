10333185fcd97dd62521021dcd2f4d65b8d5b691
Hermanest <nicedevelooper@gmail.com>
2024-09-07 23:20:13 +0300

    Added BasicEffects class with scale animation.

---
+++ ReactiveSDK/Extensions/BasicEffects.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Reactive.Components;
+
+[PublicAPI]
+public static class BasicEffects {
+    public static T WithScaleAnimation<T>(
+        this T comp,
+        Vector3 baseScale,
+        Vector3 hoverScale,
+        Optional<AnimationDuration> duration = default
+    ) where T : ButtonBase {
+        var value = ValueUtils.RememberAnimatedVector(
+            comp,
+            baseScale,
+            duration.GetValueOrDefault("200ms")
+        );
+        comp.WithListener(
+            static x => x.IsHovered,
+            x => value.Value = x ? hoverScale : baseScale
+        );
+        return comp;
+    }
+
+    public static T WithScaleAnimation<T>(
+        this T comp,
+        float baseScale,
+        float hoverScale,
+        Optional<AnimationDuration> duration = default
+    ) where T : ButtonBase {
+        return WithScaleAnimation(
+            comp,
+            Vector3.one * baseScale,
+            Vector3.one * hoverScale,
+            duration
+        );
+    }
+}
