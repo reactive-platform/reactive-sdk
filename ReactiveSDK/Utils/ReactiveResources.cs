18b5a38974239ed205d15d85f93e4616069cff4b
Hermanest <nicedevelooper@gmail.com>
2024-09-30 21:13:28 +0300

    Fixed transparent pixel sprite not being transparent.

--- ReactiveSDK/Utils/ReactiveResources.cs
+++ ReactiveSDK/Utils/ReactiveResources.cs
@@ -5,6 +5,8 @@ namespace Reactive.Components;
 public static class ReactiveResources {
     static ReactiveResources() {
         var texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, Color.clear);
+        texture.Apply();
         TransparentPixel = ReactiveUtils.CreateSprite(texture)!;
     }
 
