525a29e322b99fece0500b4e4fce9af643d7a1b3
hermanest <nicedevelooper@gmail.com>
2024-10-06 19:40:04 +0300

    Added 'StateResolutionMode' feature to 'StateColorSet'.

--- ReactiveSDK/Models/Colors/StateColorSet.cs
+++ ReactiveSDK/Models/Colors/StateColorSet.cs
@@ -4,6 +4,28 @@ using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Reactive.Components {
+    /// <summary>
+    /// Determines how <see cref="StateColorSet"/> acquires a color for an unrepresented state.
+    /// </summary>
+    [PublicAPI]
+    public enum StateResolutionMode {
+        /// <summary>
+        /// Returns the default color if not present.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Throws an exception if required state is not present.
+        /// </summary>
+        Throw,
+
+        /// <summary>
+        /// Attempts to find the closest state to the required one.
+        /// If fails returns the default color.
+        /// </summary>
+        Auto
+    }
+
     [PublicAPI]
     public class StateColorSet : IColorSet {
         public ICollection<KeyValuePair<GraphicState, Color>> States => StatesDict;
@@ -11,26 +33,28 @@ namespace Reactive.Components {
 
         public event Action? SetUpdatedEvent;
 
+        public StateResolutionMode ResolutionMode = StateResolutionMode.Auto;
+        public Color DefaultColor = Color.clear;
+
         public Color GetColor(GraphicState state) {
-            return StatesDict.TryGetValue(state, out var result) ? result : Color.clear;
+            if (StatesDict.TryGetValue(state, out var result)) {
+                return result;
+            }
+            switch (ResolutionMode) {
+                case StateResolutionMode.Default:
+                    return DefaultColor;
+                case StateResolutionMode.Auto:
+                    if (state == 0) return DefaultColor;
+                    state--;
+                    return GetColor(state);
+                case StateResolutionMode.Throw:
+                default:
+                    throw new IndexOutOfRangeException();
+            }
         }
 
         public void SetStateColor(GraphicState graphicState, Color color) {
             StatesDict[graphicState] = color;
-            SetUpdatedEvent?.Invoke();
-        }
-
-        public void ApplyColorMod(GraphicState baseState, params (GraphicState, Func<Color, Color>)[] mods) {
-            if (!StatesDict.TryGetValue(baseState, out var baseColor)) return;
-
-            foreach (var (state, modFunc) in mods) {
-                if (StatesDict.ContainsKey(state)) {
-                    StatesDict[state] = modFunc(StatesDict[state]);
-                } else {
-                    StatesDict[state] = modFunc(baseColor);
-                }
-            }
-
             SetUpdatedEvent?.Invoke();
         }
     }
