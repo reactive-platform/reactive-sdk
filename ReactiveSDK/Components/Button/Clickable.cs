60c6dca10bc6b3a4633c0bcd44129f478a3bba07
Hermanest <mutcmk@gmail.com>
2025-04-14 11:07:34 +0300

    Added Clickable component.

---
+++ ReactiveSDK/Components/Button/Clickable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Reactive.Components.Components.Button;
+
+/// <summary>
+/// A dummy button without graphic content. Will work only with a Graphic component inside it.
+/// </summary>
+[PublicAPI]
+public class Clickable : ButtonBase, ILayoutDriver {
+    #region Adapter
+
+    public ICollection<ILayoutItem> Children => _layout.Children;
+
+    public ILayoutController? LayoutController {
+        get => _layout.LayoutController;
+        set => _layout.LayoutController = value;
+    }
+
+    public void RecalculateLayout() {
+        _layout.RecalculateLayout();
+    }
+
+    #endregion
+
+    private Layout _layout = null!;
+
+    protected override GameObject Construct() {
+        return new Layout()
+            .With(x => base.Construct(x.ContentTransform))
+            .Bind(ref _layout)
+            .Use();
+    }
+}
