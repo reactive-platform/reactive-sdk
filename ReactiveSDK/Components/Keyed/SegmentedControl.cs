e597cdd96b5bf952048cc72a4ee43de74a085d48
Hermanest <mutcmk@gmail.com>
2025-06-13 20:16:18 +0300

    Added forgotten SelectedKeyChangedEvent invokation.

--- ReactiveSDK/Components/Keyed/SegmentedControl.cs
+++ ReactiveSDK/Components/Keyed/SegmentedControl.cs
@@ -36,6 +36,7 @@ namespace Reactive.Components {
 
                 _selectedKey = value;
                 WhenKeySelected?.Invoke(value);
+                _selectedKeyChanged?.Invoke(value);
                 NotifyPropertyChanged();
             }
         }
